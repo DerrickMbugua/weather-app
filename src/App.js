152e7e4a489b525054b2c3275b9bcefcbc4c2c11
Derrick Mwema <derrickmbugua50@gmail.com>
2022-07-07 08:21:21 +0300

    feat: polish

--- src/App.js
+++ src/App.js
@@ -9,20 +9,7 @@ function App() {
     long: '',
   });
 
-
-
-  //const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
   const url = `https://api.open-meteo.com/v1/forecast?latitude=${location.lat}&longitude=${location.long}&hourly=temperature_2m&current_weather=true`
-
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-      })
-      setLocation('')
-    }
-  }
 
   const handleLat = (event) => {
     event.persist();
@@ -49,40 +36,40 @@ function App() {
 
   return (
     <div className="app">
-      <div className="search">
-        <form onSubmit={handleSubmit}>
-          <input
-            value={location.lat}
-            onChange={handleLat}
-            // onKeyPress={searchLocation}
-            placeholder='Enter Latitude'
-            name='coordLat'
-            type="text" />
-          <input
-            value={location.long}
-            onChange={handleLong}
-            // onKeyPress={searchLocation}
-            name="coordLong"
-            placeholder='Enter Longitude'
-            type="text" />
-          <input type="submit" value="Submit" />
-        </form>
-
-      </div>
       <div className="container">
+        <div className="search">
+          <form onSubmit={handleSubmit}>
+            <input
+              value={location.lat}
+              onChange={handleLat}
+              placeholder='Enter Latitude'
+              name='coordLat'
+              type="text" />
+            <input
+              value={location.long}
+              onChange={handleLong}
+              name="coordLong"
+              placeholder='Enter Longitude'
+              type="text" />
+            {/* <input type="submit" value="Submit" /> */}
+            <button>Submit</button>
+          </form>
+
+        </div>
+
         <div className="top">
           <div className="location">
-            <p>{data.latitude}</p>
+            <p>Latitude: {data.latitude}</p>
+            <p>Longitude: {data.longitude}</p>
           </div>
           <div className="temp">
-            {data.current_weather ? <h1>{data.current_weather.temperature.toFixed()}°C</h1> : null}
+            {data.current_weather ? <h1>{data.current_weather.temperature.toFixed()}°F</h1> : null}
           </div>
           <div className="description">
             {data.current_weather ? <p>{data.current_weather.windspeed}</p> : null}
           </div>
         </div>
 
-        {/* {data.name !== undefined && */}
         <div className="bottom">
           <div className="feels">
             {data.current_weather ? <p className='bold'>{data.current_weather.windspeed}</p> : null}
@@ -97,8 +84,6 @@ function App() {
             <p>Wind Direction</p>
           </div>
         </div>
-        {/* } */}
-
 
 
       </div>
